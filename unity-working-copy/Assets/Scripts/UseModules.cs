4180c00ae86d4f3e8eb9a7d1205d37040d19fe9b
Martin Bo Laursen <martin.bo.laursen@lego.com>
2019-07-04 22:33:42 +0200

    Added script for using submodules.

---
+++ unity-working-copy/Assets/Scripts/UseModules.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseModules : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var output = Second.Runtime.Echo.Now("This is text.");
+        Debug.Log(output);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
